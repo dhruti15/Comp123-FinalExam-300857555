07d393094e0b06a9ac55f8824c098a03a59d2ceb
Dhrutiben Patel <dpate483@my.centennialcollege.ca>
2016-08-19 11:32:03 +0100

    third commit

--- COMP123-S2016-FinalExam/GenerateNameForm.cs
+++ COMP123-S2016-FinalExam/GenerateNameForm.cs
@@ -15,6 +15,46 @@ namespace COMP123_S2016_FinalExam
         public GenerateNameForm()
         {
             InitializeComponent();
+
+        }
+
+        private void FirstNameListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        public void GenerateNames()
+        {
+            Random random = new Random();
+            FirstNameListBox.SelectedIndex = random.Next(0, FirstNameListBox.Items.Count);
+            LastNameListBox.SelectedIndex = random.Next(0, LastNameListBox.Items.Count);
+
+            Program.character.FirstName = Convert.ToString(FirstNameListBox.SelectedItem);
+            Program.character.LastName = Convert.ToString(LastNameListBox.SelectedItem);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GenerateButton_Click(object sender, EventArgs e)
+        {
+            GenerateNames();
+        }
+
+        private void GenerateNameForm_Load(object sender, EventArgs e)
+        {
+            GenerateNames();
+        }
+
+        private void NextButton_Click(object sender, EventArgs e)
+        {
+            AbilityGeneratorForm form = new AbilityGeneratorForm();
+            form.Show();
+            this.Hide();
+            
+
         }
     }
 }
